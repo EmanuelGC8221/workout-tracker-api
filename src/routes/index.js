77c2ff55ed52f8f0a30fd842178c43b57be9019d
EmanuelGC <yoemanuelgiraldo@gmail.com>
2025-09-26 16:08:17 -0500

    añadiendo get

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,19 @@ const router = express.Router();
 
 // Importar versiones de rutas
 const v1Routes = require('./v1');
+const { version } = require('react');
 
+router.get('/', (req, res) => {
+    res.json ({
+        message: 'Workout Tracker API',
+        versions: ['v1'],
+        endpoints: {
+            v1: '/api/v1'
+        }
+    });
+});
 
-
+module.exports = router;
 
 
 
--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+// Estado en memoria (simulación)
+let users = [
+  {
+    id: "1",
+    nombre: "Emanuel Giraldo",
+    email: "Emanuelgiraldo@gmail.com",
+    creadoEn: "2025-09-12T12:00:00Z",
+    fechaUltimaActualización : "2025-09-26",
+    edad: 17,
+    alturaCm: 180,
+    pesoKg: 75,
+    rol: "user"
+    
+  }
+];
+
+
 // GET /api/v1/users
 router.get('/', (req, res) => { 
-    res.send('Get all users')
+    res.status(200).json(users);
+});
+
+// GET /users/:id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;   // 1
+  const user = users.find(u => u.id === id);   // 2
+
+  if (!user) {   // 3
+    return res.status(404).json({ error: 'Usuario no encontrado' });
+  }
+
+  res.status(200).json(user);   // 4
 });
 
 module.exports = router;
