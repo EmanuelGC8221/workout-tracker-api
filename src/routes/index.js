7d99375cdac1e7fbca5d1e65ebeb0eeac5259217
EmanuelGC <yoemanuelgiraldo@gmail.com>
2025-09-26 15:13:57 -0500

    commit 1 endpoints básicos

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// Importar versiones de rutas
+const v1Routes = require('./v1');
+
+
+
+
+
+
+
---
+++ src/routes/v1/index.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const router = express.Router();
+
+//Importar rutas específicas.
+const usersRoutes = require('./users.routes');
+
+//configurar las rutass
+router.use('/users', usersRoutes);
+
+//exportar el router principal para ser usado en app.js
+module.exports = router;
+
+
