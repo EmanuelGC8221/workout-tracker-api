7ccba7508e2075e8a2639eb0d3f95cb18b96d2d5
EmanuelGC <yoemanuelgiraldo@gmail.com>
2025-09-30 20:47:26 -0500

    commit de ejercicios.routes e import rutas especif con modulos v1

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 //Importar rutas específicas.
 const usersRoutes = require('./users.routes');
-
+const ejerciciosRoutes = require ('./ejercicios.routes');
 
 // Aquí agrupas todos los módulos de la versión v1
 router.use('/users', usersRoutes);
-
+router.use('/ejercicios', ejerciciosRoutes);
 
 //exportar el router principal para ser usado en app.js
 module.exports = router;
